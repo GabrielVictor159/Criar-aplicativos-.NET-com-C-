01d2c06912c8601c63ae4723471e06e685ef7d21
GabrielVictor159 <gabrielvictor159487@gmail.com>
2023-04-04 19:38:30 -0300

    adicionando ao repositorio

---
+++ DotNetDebugging/Program.cs
@@ -0,0 +1,24 @@
+﻿
+using System.Diagnostics;
+
+int result = Fibonacci(5);
+Console.WriteLine(result);
+
+static int Fibonacci(int n)
+{
+    Debug.WriteLine($"Entering {nameof(Fibonacci)} method");
+    Debug.WriteLine($"We are looking for the {n}th number");
+    int n1 = 0;
+    int n2 = 1;
+    int sum;
+
+    for (int i = 2; i <= n; i++)
+    {
+        sum = n1 + n2;
+        n1 = n2;
+        n2 = sum;
+        Debug.WriteLineIf(sum == 1, $"sum is 1, n1 is {n1}, n2 is {n2}");
+    }
+    Debug.Assert(n2 == 5, "The return value is not 5 and it should be.");
+    return n == 0 ? n1 : n2;
+}
